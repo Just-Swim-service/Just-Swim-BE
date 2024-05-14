6925349a35fe3c8ecae67eda2b6b91e7d075fd43
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2024-05-14 16:35:20 +0900

    comment: customer, instructor 주석 처리

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -6,6 +6,7 @@ import { Customer } from './entity/customer.entity';
 export class CustomerService {
   constructor(private readonly customerRepository: CustomerRepository) {}
 
+  /* userType을 customer로 지정할 경우 customer 정보 생성 */
   async createCustomer(userId: number): Promise<Customer> {
     try {
       return await this.customerRepository.createCustomer(userId);
@@ -14,9 +15,10 @@ export class CustomerService {
     }
   }
 
-  async findCustomer(userId: number): Promise<Customer> {
+  /* customer의 정보 조회 */
+  async findCustomerByUserId(userId: number): Promise<Customer> {
     try {
-      return await this.customerRepository.findCustomer(userId);
+      return await this.customerRepository.findCustomerByUserId(userId);
     } catch (error) {
       throw new Error('수강생 프로필 조회 중 오류가 발생했습니다.');
     }
--- src/instructor/instructor.service.ts
+++ src/instructor/instructor.service.ts
@@ -6,6 +6,7 @@ import { InstructorRepository } from './instructor.repository';
 export class InstructorService {
   constructor(private readonly instructorRepository: InstructorRepository) {}
 
+  /* userType을 instructor로 지정할 경우 instructor 정보 생성 */
   async createInstructor(userId: number): Promise<Instructor> {
     try {
       return await this.instructorRepository.createInstructor(userId);
@@ -14,9 +15,10 @@ export class InstructorService {
     }
   }
 
-  async findInstructor(userId: number): Promise<Instructor> {
+  /* instructor의 정보 조회 */
+  async findInstructorByUserId(userId: number): Promise<Instructor> {
     try {
-      return await this.instructorRepository.findInstructor(userId);
+      return await this.instructorRepository.findInstructorByUserId(userId);
     } catch (error) {
       throw new Error('강사 프로필 조회 중 오류가 발생했습니다.');
     }

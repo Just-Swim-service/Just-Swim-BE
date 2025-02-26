def3a9534bf85dbe45169f359202a8c333f28600
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-02-26 13:35:08 +0900

    build: Joi를 활용

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,20 @@ import { AwsModule } from './common/aws/aws.module';
 import { ResponseModule } from './common/response/response.module';
 import { WithdrawalReasonModule } from './withdrawal-reason/withdrawal-reason.module';
 
+import * as Joi from 'joi';
+
 @Module({
   imports: [
     LoggerModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
     }),
     // db 설정
     TypeOrmModule.forRootAsync({

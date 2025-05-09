e702097233b1e900d7d77b309c81a7211155142f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-09 17:37:26 +0900

    fix: cors 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/\.just-swim\.kr$/, 'http://localhost:3000'],
+    origin: [
+      'https://just-swim.kr',
+      'https://www.just-swim.kr',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     optionsSuccessStatus: 204,

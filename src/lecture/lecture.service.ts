34017b71553f07c8b8ed2e8bdf4a676c2963fb20
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-16 17:29:26 +0900

    fix: qr 주소 수정

--- src/lecture/lecture.service.ts
+++ src/lecture/lecture.service.ts
@@ -287,7 +287,7 @@ export class LectureService {
     // QR 생성
     try {
       const qrCodeData = await QRCode.toDataURL(
-        // BE 주소
+        // FE 주소
         `${process.env.SERVER_QR_CHECK_URI}?lectureId=${newLecture.lectureId}`,
       );
       const lectureQRCode = await this.awsService.uploadQRCodeToS3(

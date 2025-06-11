0bbe90bb3f018894c6cf0f90faba910f01778678
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-06-11 12:39:58 +0900

    test: image 컨트롤러 테스트 코드 추가

--- src/image/image.controller.spec.ts
+++ src/image/image.controller.spec.ts
@@ -1,18 +1,62 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { ImageController } from './image.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ImageController } from './image.controller';
+import { ImageService } from './image.service';
+import { ResponseService } from 'src/common/response/response.service';
+import { DeleteImageDto } from './dto/delete-image.dto';
+import { Response } from 'express';
 
-// describe('ImageController', () => {
-//   let controller: ImageController;
+describe('ImageController', () => {
+  let controller: ImageController;
+  let imageService: ImageService;
+  let responseService: ResponseService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [ImageController],
-//     }).compile();
+  const mockImageService = {
+    deleteFeedbackImageFromS3: jest.fn(),
+  };
 
-//     controller = module.get<ImageController>(ImageController);
-//   });
+  const mockResponseService = {
+    success: jest.fn(),
+  };
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ImageController],
+      providers: [
+        { provide: ImageService, useValue: mockImageService },
+        { provide: ResponseService, useValue: mockResponseService },
+      ],
+    }).compile();
+
+    controller = module.get<ImageController>(ImageController);
+    imageService = module.get<ImageService>(ImageService);
+    responseService = module.get<ResponseService>(ResponseService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('deleteImage', () => {
+    it('should delete image by URL and return success response', async () => {
+      const deleteImageDto: DeleteImageDto = {
+        fileURL:
+          'https://just-swim-bucket.s3.ap-northeast-2.amazonaws.com/feedback/1/test-image.png',
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as any as Response;
+
+      await controller.deleteImage(mockRes, deleteImageDto);
+
+      expect(imageService.deleteFeedbackImageFromS3).toHaveBeenCalledWith(
+        deleteImageDto,
+      );
+      expect(responseService.success).toHaveBeenCalledWith(
+        mockRes,
+        'feedbackImage 삭제 성공',
+      );
+    });
+  });
+});

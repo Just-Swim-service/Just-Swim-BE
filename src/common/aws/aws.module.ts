bf4dd51ca0cf1c5b15cf068362f0ea62be5d8632
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-07 13:55:59 +0900

    fix: 파일 사이즈 허용 수정

--- src/common/aws/aws.module.ts
+++ src/common/aws/aws.module.ts
@@ -6,7 +6,7 @@ import { MulterModule } from '@nestjs/platform-express';
   imports: [
     MulterModule.register({
       limits: {
-        fileSize: 20 * 1024 * 1024, // 20MB로 증가 (동영상 지원)
+        fileSize: 100 * 1024 * 1024, // 20MB로 증가 (동영상 지원)
         files: 4, // 최대 4개 파일
       },
     }),

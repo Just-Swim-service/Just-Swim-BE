0268eaeb2e1c194d7149b59b55ea9fb5068ed6bd
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-14 13:47:07 +0900

    fix: presigned url 에러 수정을 위한 조치

--- src/common/aws/aws.service.ts
+++ src/common/aws/aws.service.ts
@@ -119,6 +119,9 @@ export class AwsService {
       ? this.getContentType(`${fileName}.${ext}`)
       : this.getContentType(fileName);
 
+    console.log('[DEBUG] Presigned ContentType:', contentType);
+    console.log('[DEBUG] Presigned FileName:', fileName);
+
     const command = new PutObjectCommand({
       Bucket: this.configService.get<string>('AWS_S3_BUCKET_NAME'),
       Key: fileName,
@@ -133,6 +136,8 @@ export class AwsService {
     const presignedUrl = await getSignedUrl(this.s3Client, command, {
       expiresIn: 3600, // 1시간 유효
     });
+
+    console.log('[DEBUG] Presigned URL:', presignedUrl);
 
     return { presignedUrl, contentType };
   }

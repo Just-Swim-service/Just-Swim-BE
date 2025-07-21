08da223986b06e92832096210f9726981fec9e7a
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-21 12:14:51 +0900

    fix: presigned url content-type 부분 수정

--- src/common/aws/aws.service.ts
+++ src/common/aws/aws.service.ts
@@ -117,7 +117,9 @@ export class AwsService {
   ): Promise<{ presignedUrl: string; contentType: string }> {
     const safeFileName = fileName.normalize('NFKD').replace(/[^\w.-]/g, '_');
 
-    const contentType = this.getContentType(fileName);
+    const contentType = ext
+      ? this.getContentType(`${fileName}.${ext}`)
+      : this.getContentType(fileName);
 
     const command = new PutObjectCommand({
       Bucket: this.configService.get<string>('AWS_S3_BUCKET_NAME'),
